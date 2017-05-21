745b810623b0d614c05f5151c44a52a0de0c2df2
do duc huy <walter@trueplus.vn>
2017-05-21 08:47:07 +0700

    walter - create base

---
+++ related_field/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Related Field',
+    'version': '1.0.0',
+    'category': 'Tools',
+    'sequence': 1,
+    'author': 'Walter',
+    'website': 'http://www.magestore.com',
+    'summary': 'Related Field vs Search',
+    'description': """""",
+    'depends': [
+        'base'
+    ],
+    'data': [
+    ],
+    'sequence': 1,
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
